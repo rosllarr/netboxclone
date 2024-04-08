78dde7a5d67addbe13dcd78b2b923e66f4b1214c
rosllarr <rosllarr@tutanota.com>
2024-04-08 02:02:24 +0000

    update

--- netbox/utilities/choices.py
+++ netbox/utilities/choices.py
@@ -1,9 +1,45 @@
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+
 class ChoiceSetMeta(type):
     """
     Metaclass for ChoiceSet
     """
     def __new__(mcs, name, bases, attrs):
-        new_class = super().__new__(mcs, name, bases, attrs)
+
+        # Extend static choices with any configured choices
+        if key := attrs.get('key'):
+            assert type(attrs['CHOICES']) is list, _(
+                "{name} has a key defined but CHOICES is not a list"
+            ).format(name=name)
+            app = attrs['__module__'].split('.', 1)[0]
+            replace_key = f'{app}.{key}'
+            extend_key = f'{replace_key}+' if replace_key else None
+            if replace_key and replace_key in settings.FIELD_CHOICES:
+                # Replace the stock choices
+                attrs['CHOICES'] = settings.FIELD_CHOICES[replace_key]
+            elif extend_key and extend_key in settings.FIELD_CHOICES:
+                # Extend the stock choices
+                attrs['CHOICES'].extend(settings.FIELD_CHOICES[extend_key])
+
+        # Define choice tuples and color maps
+        attrs['_choices'] = []
+        attrs['colors'] = {}
+        for choice in attrs['CHOICES']:
+            if isinstance(choice[1], (list, tuple)):
+                grouped_choices = []
+                for c in choice[1]:
+                    grouped_choices.append((c[0], c[1]))
+                    if len(c) == 3:
+                        attrs['colors'][c[0]] = c[2]
+                attrs['_choices'].append((choice[0], grouped_choices))
+            else:
+                attrs['_choices'].append((choice[0], choice[1]))
+                if len(choice) == 3:
+                    attrs['colors'][choice[0]] = choice[2]
+
+        return super().__new__(mcs, name, bases, attrs)
 
 
 class ChoiceSet(metaclass=ChoiceSetMeta):
